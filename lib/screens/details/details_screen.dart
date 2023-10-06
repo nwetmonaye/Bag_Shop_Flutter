ef8266b63859402fcb0834809c1d6610ae8978c3
nwetmonaye <nwetmon7@gamil.com>
2023-10-06 17:17:54 +0630

    'flutter'

---
+++ lib/screens/details/details_screen.dart
@@ -0,0 +1,81 @@
+import 'package:bugshop/constants.dart';
+import 'package:bugshop/models/Product.dart';
+import 'package:bugshop/screens/details/components/color_and_size.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: product.color,
+      appBar: AppBar(
+        backgroundColor: product.color,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/back.svg',
+            colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/search.svg"),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/cart.svg"),
+            onPressed: () {},
+          ),
+          SizedBox(width: kDefaultPadding / 2)
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: size.height,
+              child: Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: size.height * 0.3),
+                    padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPadding,
+                      right: kDefaultPadding,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      ),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        ColorAndSize(product: product),
+                        SizedBox(height: kDefaultPadding / 2),
+                        // Description(product: product),
+                        // SizedBox(height: kDefaultPaddin / 2),
+                        // CounterWithFavBtn(),
+                        // SizedBox(height: kDefaultPaddin / 2),
+                        // AddToCart(product: product)
+                      ],
+                    ),
+                  ),
+                  //  ProductTitleWithImage(product: product)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,80 @@
+import 'package:bugshop/constants.dart';
+import 'package:bugshop/models/Product.dart';
+import 'package:bugshop/screens/details/details_screen.dart';
+import 'package:bugshop/screens/home/components/categories.dart';
+import 'package:bugshop/screens/home/components/item_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/back.svg"),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/search.svg",
+              colorFilter: ColorFilter.mode(kTextColor, BlendMode.srcIn),
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/cart.svg",
+              colorFilter: ColorFilter.mode(kTextColor, BlendMode.srcIn),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Text(
+              "Women",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Categories(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: kDefaultPadding,
+                  crossAxisSpacing: kDefaultPadding,
+                  childAspectRatio: 0.75,
+                ),
+                itemBuilder: (context, index) => ItemCard(
+                  product: products[index],
+                  press: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(
+                        product: products[index],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
